f24847470b0dfeb09465b02931f7ec6ad1bba3d0
Elena <helenmai@yandex.ru>
2022-07-15 21:19:28 +0300

    Ryad ot 1 do N

---
+++ Pract4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+int startnumber = 1;
+while(startnumber != (number + 1))
+{
+    Console.Write(startnumber + ",");
+    startnumber+=1;
+}
+Console.Write("\b.");
