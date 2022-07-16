169019558eea7bb05cf4eeffd45c38ecac8019c4
Elena <helenmai@yandex.ru>
2022-07-16 22:48:34 +0300

    seek third number

---
+++ Practicheskiye2/Nomer13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number< 100)
+{
+    Console.WriteLine("В заданном числе нет третьей цифры");
+}
+else
+{
+    while (number> 999)
+    {
+    number/= 10;
+    }
+    Console.WriteLine("Третья цифра " + number%10);
+}
