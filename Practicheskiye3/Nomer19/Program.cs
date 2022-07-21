92077f748f68d48cba0d788e5f8ab05b4e139f0c
Elena <helenmai@yandex.ru>
2022-07-22 00:03:05 +0300

    Proverka palindroma

---
+++ Practicheskiye3/Nomer19/Program.cs
@@ -0,0 +1,17 @@
+﻿ //Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+ //является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit1 = number/10000;
+int digit2 = (number - digit1*10000)/1000;
+int digit3 = (number - digit1*10000 - digit2*1000)/100;
+int digit4 = (number - digit1*10000 - digit2*1000 - digit3*100)/10;
+int digit5 = number - digit1*10000 - digit2*1000 - digit3*100- digit4*10;
+if (digit1 == digit5 && digit2 == digit4)
+{
+  Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом"); 
+} 
