9849d14961b6bfef43974ce6c82e185118a11bc5
Elena <helenmai@yandex.ru>
2022-07-23 13:49:12 +0300

    Sum tsifr chisla

---
+++ Practicheskiye4/Nomer27/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+void Zadacha27()
+{
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (number > 0)
+    {
+    sum = sum + number % 10;
+    number = number /10 ;
+    }
+ Console.WriteLine("Сумма цифр числа равна " + sum);
+}
+Zadacha27();
