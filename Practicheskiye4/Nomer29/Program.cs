80f39fab1745993642ebb65dab74b3c347bf9fee
Elena <helenmai@yandex.ru>
2022-07-24 00:25:45 +0300

    Sortirovka po modulyu

---
+++ Practicheskiye4/Nomer29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит отсортированный по модулю массив
+void MyArray()
+{
+    int size = 8;
+    Random random = new Random();
+    int[] array = new int[size];
+        for (int i =0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-100,10);
+    }
+    for (int i =0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            array[i] = - array[i];
+        }
+        Console.Write ("[" + array[i] + "]");
+    }
+
+}
+
+MyArray();
