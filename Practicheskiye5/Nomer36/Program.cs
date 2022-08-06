2098270fac92596364c49c159ebb6cd56ce59862
Elena <helenmai@yandex.ru>
2022-08-06 23:01:07 +0300

    Summa elementov na pozitsiyah massiva

---
+++ Practicheskiye5/Nomer36/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+int size = 10;
+int sum  = 0;
+int[] array = new int [size];
+Random random = new Random();
+    for (int i =0; i < size; i++)
+    {
+        array[i] = random.Next(-10, 10);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i =1; i < array.Length; i=i+2)
+        {
+            sum = sum + array[i];
+        }
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях, равна " + sum );
