6160700626e9b02a26e72db4ca711e8a0b6d2b07
Elena <helenmai@yandex.ru>
2022-08-09 13:34:57 +0300

    ArrayVeshchChis

---
+++ Practicheskiye5/Nomer38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+int size = 8;
+double res  = 0;
+double[] array = new double [size];
+double min = 0;
+double max = 0;
+Random random = new Random();
+void FillPrintDouble()
+    {
+    for (int i =0; i < size; i++)
+        {
+        array[i] = random.Next(-10,10) + random.NextDouble();
+        Console.Write(array[i] + " ");
+        }
+    Console.WriteLine();
+    }
+void Res()
+    {
+    for (int i = 0; i < array.Length; i++)
+        {
+        if (min > array[i])
+            {
+            min = array[i];
+            }
+        else if (max < array[i])
+            {
+            max = array[i];
+            }
+            res = max - min;
+        }
+    Console.Write("min = " + min + " max = " + max);
+    Console.WriteLine();
+    Console.WriteLine("Разница между максимальным и минимальным числами массива равна " + res);
+    }
+    FillPrintDouble();
+    Res();
