98a937131ef1361368d62531ee6f70a5d5d0ab98
Elena <helenmai@yandex.ru>
2022-08-11 10:19:45 +0300

    vvodPrintChoise

---
+++ Practicheskiye6/Nomer41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Ввeдите значение M");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [M]; 
+void Vvod()
+{
+    Console.WriteLine($"Ввeдите {M} чисел через enter");
+    for (int i = 0; i < M; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+}
+void FindPrint()
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        if (array[i] > 0)
+            {
+                count++;
+            }
+    }
+    Console.WriteLine($"Kоличество чисел больше 0 = {count}");   
+}
+Vvod();
+FindPrint();
