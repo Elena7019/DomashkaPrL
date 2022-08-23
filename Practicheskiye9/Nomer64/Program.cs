b0cc1a4118a8acbb60b9642bfa7b2ac7c39d86a3
Elena <helenmai@yandex.ru>
2022-08-23 18:17:30 +0300

    MultThree

---
+++ Practicheskiye9/Nomer64/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа кратные 3-ём 
+// в промежутке от M до N.
+
+Console.Write("Введите значение М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Натуральные числа, кратные 3, в промежутке между {M} и {N}: ");
+MultThree(M, N);
+MultThreeToo(M, N);
+void MultThree(int M, int N)
+{
+if (M<N) return;
+  if (M%3 == 0)
+    {
+      Console.Write ($"{M}" + "; ");
+      M = M-3;
+    }
+  else
+    {
+      M--;
+    }
+MultThree(M, N);
+}
+void MultThreeToo(int M, int N)
+{
+  if (N<M) return;
+  if (M%3 == 0)
+  {
+  Console.Write ($"{M}" + "; ");
+  M=M+3;
+  }
+  else
+  {
+   M++;
+  }
+  MultThreeToo
+(M, N);
+}
